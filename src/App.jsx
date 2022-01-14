2262145a9aea2ce5f1656df5a2d28289b3345160
polomas <benavidesge@outlook.com>
2022-01-14 09:31:45 -0500

    chore(Root): Add files to ignore from Git repository

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <main>
+     Tik Tok Clone
+      </main>
+    </div>
+  )
+}
+
+export default App
